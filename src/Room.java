f99828c71b364a413ec8af01e464289c26357e10
Swastika Das <swastikadas452@gmail.com>
2026-03-17 16:42:56 +0530

    Added Room class

--- src/Room.java
+++ src/Room.java
@@ -3,30 +3,25 @@ class Room {
     private boolean isAvailable;
     private String roomType;
 
-    // Constructor
     public Room(int roomNumber, String roomType) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
         this.isAvailable = true;
     }
 
-    // Book room
     public void bookRoom() {
         if (isAvailable) {
             isAvailable = false;
             System.out.println(roomType + " Room " + roomNumber + " booked successfully.");
         } else {
-            System.out.println(roomType + " Room " + roomNumber + " is not available.");
+            System.out.println("Room not available.");
         }
     }
 
-    // Free room
     public void freeRoom() {
         isAvailable = true;
-        System.out.println(roomType + " Room " + roomNumber + " is now available.");
     }
 
-    // Getters
     public int getRoomNumber() {
         return roomNumber;
     }
@@ -39,7 +34,6 @@ class Room {
         return isAvailable;
     }
 
-    // Display details
     public void displayRoom() {
         System.out.println("Room No: " + roomNumber + " | Type: " + roomType + " | Available: " + isAvailable);
     }

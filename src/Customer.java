8aeb6b372c33c54e279eabb623e4eb3787312a51
Swastika Das <swastikadas452@gmail.com>
2026-03-17 16:00:36 +0530

    Enhanced Customer class with contact details

--- src/Customer.java
+++ src/Customer.java
@@ -1,14 +1,18 @@
 class Customer {
     private int customerId;
     private String name;
+    private String phoneNumber;
+    private String email;
 
     // Constructor
-    public Customer(int customerId, String name) {
+    public Customer(int customerId, String name, String phoneNumber, String email) {
         this.customerId = customerId;
         this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
     }
 
-    // Getter methods
+    // Getters
     public int getCustomerId() {
         return customerId;
     }
@@ -17,9 +21,19 @@ class Customer {
         return name;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
     // Display customer details
     public void displayCustomer() {
         System.out.println("Customer ID: " + customerId);
-        System.out.println("Customer Name: " + name);
+        System.out.println("Name: " + name);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Email: " + email);
     }
 }
